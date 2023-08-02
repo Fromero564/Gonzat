00cefcdef7519efb9a0b61936810f6e7435c33f7
Fromero564 <fervicromero@gmail.com>
2023-08-02 19:03:18 -0300

    Se agrega cartel de bienvenida

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-alert('Hola mundo')
 
+document.addEventListener("DOMContentLoaded", function() {
+    var fadeText = document.getElementById("fade-text");
+    var fadeInDelay = 500; // Cambiado a 500ms para una aparición más rápida
+    
+    setTimeout(function() {
+        fadeText.style.opacity = "1";
+        fadeText.style.transform = "translateY(0)";
+    }, fadeInDelay);
+});
