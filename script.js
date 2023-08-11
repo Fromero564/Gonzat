58f2a567a10a297c443b584e14881ef78689b836
Fromero564 <fervicromero@gmail.com>
2023-08-11 20:15:10 -0300

    Se trabaja en carrousel

--- script.js
+++ script.js
@@ -8,3 +8,39 @@ document.addEventListener("DOMContentLoaded", function() {
         fadeText.style.transform = "translateY(0)";
     }, fadeInDelay);
 });
+
+
+var carousels = document.getElementsByClassName("proyecto-carousel");
+
+function collapseCarousel(carousel) {
+    carousel.classList.remove("proyecto-expanded");
+    document.removeEventListener("keydown", handleKeyPress);
+}
+
+function handleKeyPress(event) {
+    if (event.key === "Escape") {
+        var expandedCarousel = document.querySelector(".proyecto-carousel.proyecto-expanded");
+        if (expandedCarousel) {
+            collapseCarousel(expandedCarousel);
+        }
+    }
+}
+
+document.addEventListener("click", function (event) {
+    var target = event.target;
+    var isImage = target.tagName === "IMG";
+
+
+    if (isImage) {
+        var carousel = target.closest(".proyecto-carousel");
+        if (carousel) {
+            carousel.classList.toggle("proyecto-expanded");
+            if (carousel.classList.contains("proyecto-expanded")) {
+                document.addEventListener("keydown", handleKeyPress);
+            } else {
+                collapseCarousel(carousel);
+            }
+        }
+    }
+});
+
