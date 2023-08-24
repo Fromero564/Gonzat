bcf269ad8f4b35c5b0b3b4a164b780f998286750
JnOlivera <javiernicolasolivera@gmail.com>
2023-08-24 18:31:27 -0300

    se realizan cambios varios

--- script.js
+++ script.js
@@ -62,3 +62,16 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var scrollToTopBtn = document.querySelector(".scroll-to-top");
+  if (document.documentElement.scrollTop > 1000) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
